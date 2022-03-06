4fd0920c25b016d308d4985b174b194a9c48933f
NEUQer_xing <cx17562036369@gmail.com>
2022-03-06 17:28:53 +0800

    3-5

--- learn_OpenCV/tutorial.py
+++ learn_OpenCV/tutorial.py
@@ -1,2 +1,14 @@
 import cv2
-img = cv2.imread("")
+# 读取照片
+img = cv2.imread("Learn_Python_by_youtube/learn_OpenCV/sources/colorcolor.jpg")
+
+# img= cv2.resize(img,       (100,100))
+# #               改变的图片  大小
+img =  cv2.resize(img,       (0,0),fx = 0.5,fy = 0.5)
+
+cv2.imshow("img",img)
+#          标题   要显示的图片
+
+cv2.waitKey(0) # 图片显示时间
+
+#显示视频
